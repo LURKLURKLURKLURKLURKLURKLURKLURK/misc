66579a284defc5698ea44fdfd52e6b56a441241e
LURKLURKLURKLURKLURKLURKLURKLURK <96763530+LURKLURKLURKLURKLURKLURKLURKLURK@users.noreply.github.com>
2023-02-10 19:24:27 -0500

    Delete 5464564564341.lua

--- 5464564564341.lua
+++
@@ -1,9 +0,0 @@
-local replicated = game.GetService(game,'ReplicatedStorage')
-local real_gun_config = replicated.Modules.Config;
-local module_spoofer; module_spoofer = hookfunction(getrenv().require, function(self, ...)
-    local args = {...}
-    if self == real_gun_config then
-        return getgenv().pie_solutions_env.spoofed_config
-    end
-    return module_spoofer(self, unpack(args))
-end)
